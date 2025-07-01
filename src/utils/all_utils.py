0cad55db76600dd6ad0f54a22a923ae04c4f5f2b
ushel <utkarsh.shelke03@gmail.com>
2025-07-01 19:47:37 +0530

    stage_01 created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -8,3 +8,8 @@ def read_yaml(path_to_yaml: str) -> dict:
         content = yaml.safe_load(yaml_file)
         
     return content
+
+def create_directory(dirs:list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"Directory created at {dir_path}")
